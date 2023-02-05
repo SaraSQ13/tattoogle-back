7bf428641cf539e25b429f9928ac3f9c5bc67196
Sara <sara.s.q@hotmail.com>
2023-02-05 20:11:34 +0100

    config indexRouter

---
+++ router.js
@@ -0,0 +1,14 @@
+import { Router } from "express";
+const router = Router();
+
+
+import indexRouter from "./routes/index.js";
+
+//routes
+router.use("/", indexRouter);
+
+
+
+
+
+export default router;
