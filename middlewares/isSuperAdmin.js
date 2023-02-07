d97019f261a0f5eeac4a6d5d8befa74778e2e6fd
Sara <sara.s.q@hotmail.com>
2023-02-07 17:44:24 +0100

    fix: indent code at isSuperAdmin middleware

--- middlewares/isSuperAdmin.js
+++ middlewares/isSuperAdmin.js
@@ -1,19 +1,19 @@
 const isSuperAdmin = (req, res, next) => {
-    try {
-        if (req.user_role !== "super_admin"){
-            return res.status(401).json({
-                succes: false,
-                message: "Don't have user permission",
-            });
-        }
-
-        next();
-    } catch (error){
-        return res.status(500).json({
-            success: false,
-            message: "Don't have user permission",
-        });
+  try {
+    if (req.user_role !== "super_admin") {
+      return res.status(401).json({
+        succes: false,
+        message: "Don't have user permission",
+      });
     }
+
+    next();
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Don't have user permission",
+    });
+  }
 };
 
 export default isSuperAdmin;
