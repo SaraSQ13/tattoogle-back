ee831ea06672043f1690e32cd3206f91aa5ca44e
Sara <sara.s.q@hotmail.com>
2023-02-07 18:31:54 +0100

    feat: created auth routes

--- router.js
+++ router.js
@@ -2,8 +2,12 @@ import { Router } from "express";
 const router = Router();
 
 import indexRouter from "./routes/index.js";
+import authRouter from "./routes/auth.js";
 
 //routes
 router.use("/", indexRouter);
+
+//auth
+router.use("/auth", authRouter);
 
 export default router;
