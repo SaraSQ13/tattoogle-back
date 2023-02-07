c67de86c1a6087b05911e8c51104ab646cac2251
Sara <sara.s.q@hotmail.com>
2023-02-07 14:46:42 +0100

    feat: created validateToken at AuthController

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -40,42 +40,55 @@ AuthController.register = async (req, res) => {
   }
 };
 
-AuthController.login = async (req, res) =>{
-    try {
-        const {email, password} = req.body;
+AuthController.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
 
-        if(!email || !password) {
-            return res.status(400).json({
-                success: false,
-                message: "email and password are required",
-            });
-        }
-        const user = await User.findOne({email:email});
-        const isValidPassword = compareSync(password,user.password);
-
-        if(!isValidPassword){
-            return res.status(401).json({
-                success: false,
-                message: "Bad credentials",
-            });
-        }
-
-        const token = jwt.sign(
-            {user_id: user._id, user_role: user.role},
-            process.env.JWT_SECRET,
-            {expiresIn: "20m"}
-        );
-
-        return res.status(200).json({
-            success: true,
-            message: `User Logged as ${user.role.toUpperCase()}`,
-            token:token,
-            user: user,
-        });
-    } catch (error) {
-        return res.status(500).json({
-            success: false,
-            message: "User login failed",
-        });
+    if (!email || !password) {
+      return res.status(400).json({
+        success: false,
+        message: "email and password are required",
+      });
     }
+    const user = await User.findOne({ email: email });
+    const isValidPassword = compareSync(password, user.password);
+
+    if (!isValidPassword) {
+      return res.status(401).json({
+        success: false,
+        message: "Bad credentials",
+      });
+    }
+
+    const token = jwt.sign(
+      { user_id: user._id, user_role: user.role },
+      process.env.JWT_SECRET,
+      { expiresIn: "20m" }
+    );
+
+    return res.status(200).json({
+      success: true,
+      message: `User Logged as ${user.role.toUpperCase()}`,
+      token: token,
+      user: user,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "User login failed",
+    });
+  }
 };
+
+AuthController.validateToken = async (req, res) => {
+  try {
+    const { token } = req.body;
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    return res.json({ isValid: true, payload: decoded });
+  } catch (error) {
+    return res.json({ isValid: false });
+  }
+};
+
+export default AuthController;
