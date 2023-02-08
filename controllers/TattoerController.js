6ba2eeae853a5c0dd344d65dd24830d0211fced5
Sara <sara.s.q@hotmail.com>
2023-02-08 19:51:36 +0100

    feat: created function get all tattoers at TattoerController

---
+++ controllers/TattoerController.js
@@ -0,0 +1,22 @@
+import Tattoer from "../models/Tattoer.js";
+
+const TattoerController = {};
+
+// Get all tattoers
+TattoerController.getAll = async (req, res) => {
+  try {
+    const tattoer = await Tattoer.find();
+
+    return res.status(200).json({
+      success: true,
+      message: " Get all tattoers retrieved successfully",
+      data: tattoer,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Error retrieving tattoers",
+      error: error.message,
+    });
+  }
+};
