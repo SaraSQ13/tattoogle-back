b6b214ae8c6c8af1ffcb49c07cc75e158ebcd813
Sara <sara.s.q@hotmail.com>
2023-02-09 15:18:07 +0100

    feat: created function get all styles & get style by name at StyleController

---
+++ controllers/StyleController.js
@@ -0,0 +1,44 @@
+import Studio from "../models/Studio.js";
+
+const StyleController = {};
+
+// Get all studios
+StyleController.getAll = async (req, res) => {
+  try {
+    const style = await Studio.find();
+
+    return res.status(200).json({
+      success: true,
+      message: " Get all styles retrieved successfully",
+      data: style,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Error retrieving style",
+      error: error.message,
+    });
+  }
+};
+
+// Get studio by name
+
+StyleController.getByName = async (req, res) => {
+  try {
+    const style = await Studio.findOne({ style: req.params.style });
+
+    return res.status(200).json({
+      success: true,
+      message: "Get style successfully",
+      data: style,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Error retrieving style",
+      error: error.messag,
+    });
+  }
+};
+
+export default StyleController;
