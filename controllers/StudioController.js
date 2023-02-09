0d8a8d0dd9a829a12520f1490b8c3f920abe1859
Sara <sara.s.q@hotmail.com>
2023-02-09 13:31:52 +0100

    feat: created function get all studios and get studio by name at StudioController

--- controllers/StudioController.js
+++ controllers/StudioController.js
@@ -1,6 +1,5 @@
 import Studio from "../models/Studio.js";
 
-
 const StudioController = {};
 
 // Get all studios
@@ -22,25 +21,24 @@ StudioController.getAll = async (req, res) => {
   }
 };
 
-
 // Get studio by name
 
 StudioController.getByName = async (req, res) => {
-    try {
-        const studio = await Studio.findOne({name: req.params.name});
+  try {
+    const studio = await Studio.findOne({ name: req.params.name });
 
-        return res.status(200).json({
-            success: true,
-            message: "Get studio successfully",
-            data: studio,
-        });
-    } catch (error) {
-        return res.status(500).json({
-            success: false,
-            message: "Error retrieving studio",
-            error: error.messag,
-        });
-    }
+    return res.status(200).json({
+      success: true,
+      message: "Get studio successfully",
+      data: studio,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Error retrieving studio",
+      error: error.messag,
+    });
+  }
 };
 
 export default StudioController;
